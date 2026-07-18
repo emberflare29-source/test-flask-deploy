2e05f946b1c714c750c15923cd302f2ef1035610
Mowit <mowit@email.com>
2026-07-18 11:40:55 +0500

    for back4app

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -30,4 +31,6 @@ def parse_float_params(name, value):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    # app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
